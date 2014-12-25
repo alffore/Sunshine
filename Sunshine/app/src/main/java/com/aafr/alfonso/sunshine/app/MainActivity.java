e0a47a8bd5f8233a42613e08292cef7f660f88cf
A. Alfonso F. R <alffore@gmail.com>
2014-12-25 13:39:32 -0600

    Se agrega un ListView
    Se agrega las partes fundamentales del ListView y se conecta para que
    renderee un array estático.

--- Sunshine/app/src/main/java/com/aafr/alfonso/sunshine/app/MainActivity.java
+++ Sunshine/app/src/main/java/com/aafr/alfonso/sunshine/app/MainActivity.java
@@ -10,6 +10,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -53,13 +59,32 @@ public class MainActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
+        ArrayAdapter<String> mForecastAdapter;
+
         public PlaceholderFragment() {
-        }
+            }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            String[] forecastArray={
+                    "Today - Sunny - 88/63",
+                    "Today - Sunny - 88/63"
+            };
+
+            List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
+
+
+            mForecastAdapter =new ArrayAdapter<String>(getActivity(),
+                    R.layout.list_item_forecast,R.id.list_item_forecast_textview,weekForecast);
+
+            ListView lv =(ListView)rootView.findViewById(R.id.listview_forecast);
+
+            lv.setAdapter(mForecastAdapter);
+
             return rootView;
         }
     }
