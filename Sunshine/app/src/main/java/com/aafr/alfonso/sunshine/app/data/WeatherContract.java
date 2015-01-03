a2fb8aa8b778258c4ee10550e4b147b0dd69f39e
A. Alfonso F. R <alffore@gmail.com>
2015-01-03 12:55:06 -0600

    Se conecta la BD a la recuperación de JSON
    , se reconstruye  la interfaz y se desconecta el pintado después de la
    recuperación, se utilzan los Cursores y Loaders para el Adapter

--- Sunshine/app/src/main/java/com/aafr/alfonso/sunshine/app/data/WeatherContract.java
+++ Sunshine/app/src/main/java/com/aafr/alfonso/sunshine/app/data/WeatherContract.java
@@ -5,11 +5,16 @@ import android.content.ContentUris;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by alfonso on 29/12/14.
  *
  * Defines table and column names for the weather database.
  */
+
 public class WeatherContract {
 
     // The "Content authority" is a name for the entire content provider, similar to the
@@ -29,6 +34,41 @@ public class WeatherContract {
     // At least, let's hope not.  Don't be that dev, reader.  Don't be that dev.
     public static final String PATH_WEATHER = "weather";
     public static final String PATH_LOCATION = "location";
+
+    // Format used for storing dates in the database.  ALso used for converting those strings
+    // back into date objects for comparison/processing.
+    public static final String DATE_FORMAT = "yyyyMMdd";
+
+    /**
+     * Converts Date class to a string representation, used for easy comparison and database lookup.
+     * @param date The input date
+     * @return a DB-friendly representation of the date, using the format defined in DATE_FORMAT.
+     */
+    public static String getDbDateString(Date date){
+        // Because the API returns a unix timestamp (measured in seconds),
+        // it must be converted to milliseconds in order to be converted to valid date.
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
+        return sdf.format(date);
+    }
+
+
+
+    /**
+     * Converts a dateText to a long Unix time representation
+     * @param dateText the input date string
+     * @return the Date object
+     */
+    public static Date getDateFromDb(String dateText) {
+        SimpleDateFormat dbDateFormat = new SimpleDateFormat(DATE_FORMAT);
+        try {
+            return dbDateFormat.parse(dateText);
+        } catch ( ParseException e ) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
 
     /* Inner class that defines the table contents of the location table */
     public static final class LocationEntry implements BaseColumns {
@@ -57,12 +97,9 @@ public class WeatherContract {
         public static final String COLUMN_COORD_LAT = "coord_lat";
         public static final String COLUMN_COORD_LONG = "coord_long";
 
-
-        public static Uri buildLocationUri(long id){
-    return ContentUris.withAppendedId(CONTENT_URI, id);
-}
-
-
+        public static Uri buildLocationUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
     }
 
     /* Inner class that defines the table contents of the weather table */
