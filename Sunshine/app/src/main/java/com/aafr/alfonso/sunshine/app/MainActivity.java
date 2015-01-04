2373530524e6d5b123731ef4a0fbf519663346cc
A. Alfonso F. R <alffore@gmail.com>
2015-01-04 13:44:28 -0600

    Se agega y generaliza el layout para tablets
    Se conectan ls aplicaciones y se cambia el comportamiento de como se
    comunican

--- Sunshine/app/src/main/java/com/aafr/alfonso/sunshine/app/MainActivity.java
+++ Sunshine/app/src/main/java/com/aafr/alfonso/sunshine/app/MainActivity.java
@@ -10,20 +10,32 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+public class MainActivity extends ActionBarActivity implements ForecastFragment.Callback {
 
-public class MainActivity extends ActionBarActivity {
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
-    private final String LOG_TAG=MainActivity.class.getSimpleName();
-
+    private boolean mTwoPane;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new ForecastFragment())
-                    .commit();
+        if (findViewById(R.id.weather_detail_container) != null) {
+            // The detail container view will be present only in the large-screen layouts
+            // (res/layout-sw600dp). If this view is present, then the activity should be
+            // in two-pane mode.
+            mTwoPane = true;
+
+            // In two-pane mode, show the detail view in this activity by
+            // adding or replacing the detail fragment using a
+            // fragment transaction.
+            if (savedInstanceState == null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.weather_detail_container, new DetailFragment())
+                        .commit();
+            }
+        } else {
+            mTwoPane = false;
         }
     }
 
@@ -31,7 +43,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -41,57 +53,60 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-
-
-            startActivity(new Intent(this,SettingsActivity.class));
-
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
-
-        if(id == R.id.action_map){
-
+        if (id == R.id.action_map) {
             openPreferredLocationInMap();
             return true;
-
         }
-
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * Metodo que recupera la localizacion de las preferencias
-     */
-    private void openPreferredLocationInMap(){
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String location=sharedPreferences.getString(getString(R.string.pref_location_key),
+    private void openPreferredLocationInMap() {
+        SharedPreferences sharedPrefs =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        String location = sharedPrefs.getString(
+                getString(R.string.pref_location_key),
                 getString(R.string.pref_location_default));
 
-        final String FORECAST_BASE_URL= "geo:0,0?";
-        final String QUERY_PARAM="q";
-
-        Uri geoLocation = Uri.parse(FORECAST_BASE_URL).buildUpon()
-                .appendQueryParameter(QUERY_PARAM, location).build();
-
-        showMap(geoLocation);
-    }
-
-
-    /**
-     * Metodo para mostrar la geolocalizacion
-     * @param geoLocation
-     */
-    public void showMap(Uri geoLocation) {
+        // Using the URI scheme for showing a location found on a map.  This super-handy
+        // intent can is detailed in the "Common Intents" page of Android's developer site:
+        // http://developer.android.com/guide/components/intents-common.html#Maps
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build();
 
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(geoLocation);
+
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
-        }else{
-            Log.d(LOG_TAG,"Gmaps: "+"no se puede abrir app");
+        } else {
+            Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
         }
     }
 
+    @Override
+    public void onItemSelected(String date) {
+        if (mTwoPane) {
+            // In two-pane mode, show the detail view in this activity by
+            // adding or replacing the detail fragment using a
+            // fragment transaction.
+            Bundle args = new Bundle();
+            args.putString(DetailActivity.DATE_KEY, date);
+
+            DetailFragment fragment = new DetailFragment();
+            fragment.setArguments(args);
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.weather_detail_container, fragment)
+                    .commit();
+        } else {
+            Intent intent = new Intent(this, DetailActivity.class)
+                    .putExtra(DetailActivity.DATE_KEY, date);
+            startActivity(intent);
+        }
+    }
 }
--- Sunshine/app/src/main/java/com/aafr/alfonso/sunshine/app/SettingsActivity.java
+++ Sunshine/app/src/main/java/com/aafr/alfonso/sunshine/app/SettingsActivity.java
@@ -1,17 +1,16 @@
 package com.aafr.alfonso.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
+
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
-import android.view.KeyEvent;
+
 
 import com.aafr.alfonso.sunshine.app.data.WeatherContract;
 
@@ -95,4 +94,9 @@ public class SettingsActivity extends PreferenceActivity
         return true;
     }
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public Intent getParentActivityIntent() {
+        return super.getParentActivityIntent().addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    }
 }
