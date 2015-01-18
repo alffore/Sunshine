d42994b3d69c8b364bb22f65c0a63bd8cc49c683
A. Alfonso F. R <alffore@gmail.com>
2015-01-18 12:43:27 -0600

    Se pone servicio y alarma y Async

--- Sunshine/app/src/main/java/com/aafr/alfonso/sunshine/app/ForecastFragment.java
+++ Sunshine/app/src/main/java/com/aafr/alfonso/sunshine/app/ForecastFragment.java
@@ -15,6 +15,10 @@
  */
 package com.aafr.alfonso.sunshine.app;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -34,6 +38,7 @@ import android.widget.ListView;
 import com.aafr.alfonso.sunshine.app.data.WeatherContract;
 import com.aafr.alfonso.sunshine.app.data.WeatherContract.LocationEntry;
 import com.aafr.alfonso.sunshine.app.data.WeatherContract.WeatherEntry;
+import com.aafr.alfonso.sunshine.app.service.SunshineService;
 
 import java.util.Date;
 
@@ -171,8 +176,16 @@ public class ForecastFragment extends Fragment implements LoaderCallbacks<Cursor
     }
 
     private void updateWeather() {
-        String location = Utility.getPreferredLocation(getActivity());
-        new FetchWeatherTask(getActivity()).execute(location);
+        Intent alarmIntent = new Intent(getActivity(), SunshineService.AlarmReceiver.class);
+        alarmIntent.putExtra(SunshineService.LOCATION_QUERY_EXTRA, Utility.getPreferredLocation(getActivity()));
+
+        //Wrap in a pending intent which only fires once.
+        PendingIntent pi = PendingIntent.getBroadcast(getActivity(), 0,alarmIntent,PendingIntent.FLAG_ONE_SHOT);//getBroadcast(context, 0, i, 0);
+
+        AlarmManager am=(AlarmManager)getActivity().getSystemService(Context.ALARM_SERVICE);
+
+        //Set the AlarmManager to wake up the system.
+        am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5000, pi);
     }
 
     @Override
